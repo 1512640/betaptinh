31e2d8c4e8d77d2ecb9e410d354b70887bd5e253
1512640 <1512640@student.hcmus.edu.vn>
2018-04-26 21:28:07 +0700

    repair
    #2

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Bé tập tính', s1: '', s2: '', kq: '' });
-  req.session.errors = null;
+  res.render('home', { title: 'Bé tập tính', s1: '', s2: '', kq: ''});
+
 });
 /*router.get('/:id1 :id2', function(req, res, next) {
     res.render('/', {s1: req.params.expr,s1: req.params.expr1});
 });*/
-router.post('/', function(req, res, next) {
-
+router.post('/', function(req, res, next){
   var kq = '';
   var s1 = req.body.expr;
-  s1 = parseFloat(s1, 10);
   var s2 = req.body.expr1;
-  s2 = parseFloat(s2, 10);
+  var loiSo1='';
+  var loi1=true;
+  var loiSo2='';
+  var loi2=true;
+  //kiem tra la so
+  if (isNaN(s1)) {
+      loiSo1 = 'Số thứ nhất phải là số';
+      loi1=false;
+  }
+  else {
+      s1 = parseFloat(s1, 10);
+  }
+  if (isNaN(s2)){
+      loiSo2 = 'Số thứ hai  phải là số';
+      loi2=false;
+  }
+  else{
+      s2 = parseFloat(s2, 10);
+  }
   var radio;
   var loiPhepTinh = true;
   radio = req.body.phepToan;
@@ -49,7 +65,8 @@ router.post('/', function(req, res, next) {
       }
       ;
   }
-  res.render('home',{s1: s1,s2: s2, kq: kq});
+
+  res.render('home',{s1,s2, kq,loiPhepTinh,thongBao,loiSo1,loi1,loiSo2,loi2});
 });
 
 module.exports = router;
